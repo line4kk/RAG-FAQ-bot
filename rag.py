85d8369a9c3a312231a962eb0dfa19a3f6700ea3
line4kk's book <silandr2016@gmail.com>
2025-10-18 21:20:16 +0400

    before

--- rag.py
+++ rag.py
@@ -2,6 +2,7 @@ from google import genai
 from google.genai import types
 from sentence_transformers import SentenceTransformer
 import faiss
+import numpy as np
 
 from dotenv import find_dotenv, load_dotenv
 import os
@@ -11,7 +12,7 @@ class RAG:
     def __init__(self, basic_prompt : str, faq : dict):
         self.__ai_client = genai.Client(api_key=self.__get_api_key())
         self.__basic_prompt = basic_prompt  # Базовый промпт для ИИ, чтобы он понимал кто он. Должен заканчиваться на просьбу ответить на следующий вопрос и последующим двоеточием
-        self.__embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.__embedding_model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
         self.__faq : dict = {}  # Кеш для FAQ
         self.__questions : list = []  # Список вопросов
         self.__L1_search = None  # L1 поисковик
@@ -28,15 +29,21 @@ class RAG:
         if not question:
             raise ValueError("Пустой вопрос")
         vector_question = self.__embedding_model.encode([question], convert_to_numpy=True)
+        vector_question = np.ascontiguousarray(vector_question.astype('float32'))
+        faiss.normalize_L2(vector_question)
         distances, indices = self.__L1_search.search(vector_question, count)
         results = [self.__questions[i] for i in indices[0]]
+        print(results)
         return results
 
     def __update_vector_faq(self):
         vector_questions = self.__embedding_model.encode(self.__questions, convert_to_numpy=True)
+        vector_questions = np.ascontiguousarray(vector_questions.astype('float32'))
+        faiss.normalize_L2(vector_questions)
         dimension = vector_questions.shape[1]
-        self.__L1_search = faiss.IndexFlatL2(dimension)
+        self.__L1_search = faiss.IndexFlatIP(dimension)
         self.__L1_search.add(vector_questions)
+        print(f"Vector question = {vector_questions}")
 
     # Добавить новую пару вопрос-ответ в кеш
     def add_aq(self, question : str, answer : str):
@@ -58,18 +65,18 @@ class RAG:
         self.__questions = list(faq.keys())
         self.__update_vector_faq()
     
-    def get_responce(self, text : str, model : str="gemini-2.5-flash", accuracy=2) -> str:
+    def get_response(self, text : str, model : str="gemini-2.5-flash", accuracy=2) -> str:
         if not self.__faq:
             raise RuntimeError("Не установлена база FAQ")
-        similary_questions = self.__find_similar_questions(text)
-        prompt = self.__basic_prompt + " " + text + f"Используй ИСКЛЮЧИТЕЛЬНО следующие данные о вопросах и ответах на них:"
-        for question in similary_questions:
-            prompt += f"\nВопрос: {question}; Ответ: {self.__faq[question]}"
-        response = self.__ai_client.models.generate_content(
-            model=model,
-            contents=prompt,
-            config=types.GenerateContentConfig(thinking_config=types.ThinkingConfig(thinking_budget=0)))
+        similary_questions = self.__find_similar_questions(text, count=accuracy)
+        # prompt = self.__basic_prompt + " " + text + f"Используй ИСКЛЮЧИТЕЛЬНО следующие данные о вопросах и ответах на них:"
+        # for question in similary_questions:
+        #     prompt += f"\nВопрос: {question}; Ответ: {self.__faq[question]}"
+        # response = self.__ai_client.models.generate_content(
+        #     model=model,
+        #     contents=prompt,
+        #     config=types.GenerateContentConfig(thinking_config=types.ThinkingConfig(thinking_budget=0)))
 
-        return response.text
+        # return response.text
     
 
