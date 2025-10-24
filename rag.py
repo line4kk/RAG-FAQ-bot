5e65a0ff7d46d2f82d09a212f3978632c4bf4164
line4kk's book <silandr2016@gmail.com>
2025-10-24 20:59:04 +0400

    RAG класс обновлен под использование RetrievalAugmented
    Добавлен геттер data_dict в RetrievalAugmented

--- rag.py
+++ rag.py
@@ -157,6 +157,12 @@ class RetrievalAugmented:
         self.__L1_search = faiss.IndexFlatIP(dimension)
         self.__L1_search.add(embedding)
 
+    def get_dict(self):
+        """
+        Получение словаря FAQ
+        """
+        return self.__data_dict
+
     def set_dict(self, dictionary: Dict[str, str]) -> None:
         """
         Обновление словаря FAQ.
@@ -206,15 +212,16 @@ class RetrievalAugmented:
 
         return retrieval_result if retrieval_result else []
 
-
-
 class RAG:
-    def __init__(self, basic_prompt : str, faq : dict):
+    def __init__(self, basic_prompt : str, faq : dict, retrieval : RetrievalAugmented) -> None:
+        if retrieval.get_dict() != faq:
+            raise ValueError("Словарь, переданный в конструкторе, отличается от словаря в RetrievalAugmented.")
+
         self.__ai_client = genai.Client(api_key=self.__get_api_key())
         self.__basic_prompt = basic_prompt  # Базовый промпт для ИИ, чтобы он понимал кто он. Должен заканчиваться на просьбу ответить на следующий вопрос и последующим двоеточием
         self.__faq : dict = {}  # Кеш для FAQ
         self.__questions: list = []  # Список вопросов
-        self.__L1_search = None  # L1 поисковик
+        self.__retrieval_augmented = retrieval  # Объект класса RetrievalAugmented
 
         self.set_faq(faq)
 
@@ -228,12 +235,11 @@ class RAG:
         if not isinstance(question, str) or not isinstance(answer, str):
             raise TypeError("Ключ и значение должны быть строками.")
         if not question or not answer:
-            raise ValueError("Пустой вопрос или ответ")
+            raise ValueError("Пустой вопрос или ответ.")
         if question in self.__faq:
             raise ValueError("Такой вопрос уже есть.")
         self.__faq[question] = answer
         self.__questions.append(question)
-        self.__update_vector_faq()
 
     # Кэшировать FAQ
     def set_faq(self, faq : dict) -> None:
@@ -241,15 +247,12 @@ class RAG:
             raise ValueError("Пустой словарь")
         self.__faq = faq
         self.__questions = list(faq.keys())
-        self.__update_vector_faq()
-    
+
     def get_response(self, text : str, model : str="gemini-2.5-flash", accuracy=2) -> str:
         if not self.__faq:
             raise RuntimeError("Не установлена база FAQ")
-        similary_questions = self.__find_similar_questions(text)
         prompt = self.__basic_prompt + " " + text + f"Используй ИСКЛЮЧИТЕЛЬНО следующие данные о вопросах и ответах на них:"
-        for question in similary_questions:
-            prompt += f"\nВопрос: {question}; Ответ: {self.__faq[question]}"
+
         response = self.__ai_client.models.generate_content(
             model=model,
             contents=prompt,
