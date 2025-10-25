8f2847c7f3f059fd9a4533322cf6bfe2f1de5b20
line4kk's book <silandr2016@gmail.com>
2025-10-25 14:23:40 +0400

    __get_api_key теперь статический и выбрасывает исключение в случае отсутствия переменной окружения
    prompt в get_response теперь собирается через join

--- RAG/ra_generation.py
+++ RAG/ra_generation.py
@@ -15,25 +15,33 @@ class RAGeneration(RetrievalAugmented):
         self.__basic_prompt = basic_prompt  # Базовый промпт для ИИ, чтобы он понимал кто он (контекст)
 
     # Получить ключ API из env
+    @staticmethod
     def __get_api_key(self) -> str:
         load_dotenv(find_dotenv())
-        return os.getenv("GEMINI_API_KEY")
-
+        key = os.getenv("GEMINI_API_KEY")
+        if not key:
+            raise RuntimeError("Не найден GEMINI_API_KEY в .env")
+        return key
 
     def get_response(self, text : str, model : str="gemini-2.5-flash", kw_res_max_len=2, emb_res_max_len=3) -> str:
         if not self._data_dict:
             raise RuntimeError("Не установлена база FAQ")
         similar_questions = super().retrieval(text, kw_res_max_len, emb_res_max_len)
-        prompt = self.__basic_prompt + '\n'
-        prompt += "Вопрос пользователя: " + text + "\n"
-        prompt += "Из базы данных FAQ были найдены похожие на вопрос пользователя результаты. Сейчас я их тебе перечислю. Проанализируй их и выбери только тот (те), что действительно подходит(ят) под вопрос пользователя и отвечают на него. Затем, сформулируй ответ используя только его (их). Иначе - попроси пользователя переформулировать вопрос.\n"
+        parts = [
+            self.__basic_prompt,
+            f"Вопрос пользователя: {text}",
+            "Из базы FAQ найдены похожие вопросы. "
+            "Проанализируй их и выбери те, что действительно отвечают на вопрос пользователя. "
+            "Если подходящих нет — попроси пользователя переформулировать запрос."
+        ]
         if similar_questions:
-            for question in similar_questions:
-                prompt += '\n'
-                prompt += "Вопрос: " + question + "\n"
-                prompt += "Ответ: " + self._data_dict[question] + "\n"
+            for q in similar_questions:
+                a = self._data_dict[q]
+                parts.append(f"\nВопрос: {q}\nОтвет: {a}")
         else:
-            prompt += "Результаты в FAQ не найдены."
+            parts.append("Результаты не найдены в базе часто-задаваемых вопросов.")
+
+        prompt = "\n".join(parts)
 
         response = self.__ai_client.models.generate_content(
             model=model,
