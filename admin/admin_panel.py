4ae8767d75cc7bd320d71f289ac9e1997212e179
line4kk <silandr2016@gmail.com>
2025-11-02 13:42:49 +0400

    Вход в админ панель и выход Теперь id админов хранится в строковом представлении

--- admin/admin_panel.py
+++ admin/admin_panel.py
@@ -1,4 +1,33 @@
-from aiogram import types
+from aiogram import types, Router
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
 
-async def admin_panel(message: types.Message):
-    pass
+from filters.admin_filter import IsAdmin
+
+admin_panel_rout = Router()
+admin_panel_rout.message.filter(IsAdmin())
+
+
+class AdminStates(StatesGroup):
+    admin_panel_state = State()
+
+
+@admin_panel_rout.message(Command("admin"))
+async def start_admin_panel(message: types.Message, state: FSMContext):
+    await message.answer(
+        "Вы перешли в панель администратора. Доступные команды:\n\n"
+        "/download - загрузить новую базу данных FAQ\n"
+        "/exit - выйти из панели администратора"
+    )
+    await state.set_state(AdminStates.admin_panel_state)
+
+
+@admin_panel_rout.message(Command("exit"))
+async def cancel_state(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if not current_state:
+        return
+    await state.clear()
+    await message.answer("Вы вышли из панели администратора")
+
--- create_bot.py
+++ create_bot.py
@@ -3,8 +3,10 @@ from aiogram import Bot, Dispatcher
 from dotenv import find_dotenv, load_dotenv
 
 from handlers.start_handler import start_rout
+from admin.admin_panel import admin_panel_rout
 
 dp = Dispatcher()
+dp.include_router(admin_panel_rout)
 dp.include_router(start_rout)
 
 load_dotenv(find_dotenv())
